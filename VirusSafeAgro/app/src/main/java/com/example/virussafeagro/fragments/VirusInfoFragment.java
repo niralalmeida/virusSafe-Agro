b6ca3c5d4118239f137b01a58a7aaeece59be988
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-25 18:34:06 +0800

    Add Virus Info Fragment
    - add java file
    - add layout file

---
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/VirusInfoFragment.java
@@ -0,0 +1,33 @@
+package com.example.virussafeagro.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.virussafeagro.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * @author Haoyu Yang
+ */
+public class VirusInfoFragment extends Fragment {
+    private View view;
+
+    public VirusInfoFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the View for this fragment
+        this.view = inflater.inflate(R.layout.fragment_virus_info, container, false);
+        return this.view;
+    }
+
+}
