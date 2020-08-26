9c6301d0e1364238b5592dcbae261518cc7da003
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-26 23:25:31 +0800

    Create RecyclerView in VirusQuizListFragment

--- VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/VirusQuizListFragment.java
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/VirusQuizListFragment.java
@@ -8,8 +8,18 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.virussafeagro.R;
+import com.example.virussafeagro.adapters.VirusInfoListAdapter;
+import com.example.virussafeagro.adapters.VirusQuizListAdapter;
+import com.example.virussafeagro.entities.Virus;
+import com.example.virussafeagro.uitilities.FragmentOperator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fragment with virus list for quiz
@@ -17,6 +27,11 @@ import com.example.virussafeagro.R;
  */
 public class VirusQuizListFragment extends Fragment {
     private View view;
+
+    private RecyclerView.LayoutManager layoutManager;
+    private VirusQuizListAdapter virusQuizListAdapter;
+    private RecyclerView recyclerViewForVirusQuizList;
+    private List<Virus> virusQuizList = new ArrayList<>();
 
     public VirusQuizListFragment() {
     }
@@ -32,6 +47,37 @@ public class VirusQuizListFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+    }
+
+    private void showVirusQuizListRecyclerView() {
+        virusQuizListAdapter = new VirusQuizListAdapter(virusQuizList, requireActivity());
+        recyclerViewForVirusQuizList = view.findViewById(R.id.rv_virus_quiz);
+        recyclerViewForVirusQuizList.addItemDecoration(new DividerItemDecoration(requireActivity(), LinearLayoutManager.VERTICAL));
+        recyclerViewForVirusQuizList.setAdapter(virusQuizListAdapter);
+        layoutManager = new LinearLayoutManager(requireActivity());
+        recyclerViewForVirusQuizList.setLayoutManager(layoutManager);
+    }
+
+    private void setRecyclerViewItemTakeQuizButtonClickListener(){
+        virusQuizListAdapter.setOnVirusQuizButtonClickListener(position -> {
+            Bundle bundle = new Bundle();
+            Virus currentVirus = virusQuizList.get(position);
+            bundle.putParcelable("currentVirus", currentVirus);
+//            VirusDetailFragment virusDetailFragment = new VirusDetailFragment();
+//            virusDetailFragment.setArguments(bundle);
+//            FragmentOperator.replaceFragmentNoBackStack(requireActivity(), virusDetailFragment);
+        });
+    }
+
+    private void setRecyclerViewItemViewContentButtonClickListener(){
+        virusQuizListAdapter.setOnVirusViewContentButtonClickListener(position -> {
+            Bundle bundle = new Bundle();
+            Virus currentVirus = virusQuizList.get(position);
+            bundle.putParcelable("currentVirus", currentVirus);
+            VirusDetailFragment virusDetailFragment = new VirusDetailFragment();
+            virusDetailFragment.setArguments(bundle);
+            FragmentOperator.replaceFragmentNoBackStack(requireActivity(), virusDetailFragment);
+        });
     }
 
 }
