4778031debad09181077bb01072ecce61ebfedf7
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-27 20:56:49 +0800

    Add Sql script about adding recodes for testing

---
+++ SQL script/Test-add question record.sql
@@ -0,0 +1,10 @@
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    1,
+    'single question 1',
+    's',
+    'sym 3',
+    'spread 3',
+    'pppp3',
+    'ddddd3'
+);
