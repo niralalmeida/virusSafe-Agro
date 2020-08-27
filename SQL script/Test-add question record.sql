fce0ce30efd3a33718b2490c9f18c3a362db1b29
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-27 21:05:24 +0800

    Modify the Sql script for testing

--- SQL script/Test-add question record.sql
+++ SQL script/Test-add question record.sql
@@ -1,10 +1,191 @@
+-- add for virus 1
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     1,
-    'single question 1',
+    'single question 1.1',
     's',
-    'sym 3',
-    'spread 3',
-    'pppp3',
-    'ddddd3'
+    'B',
+    1
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    2,
+    'single question 1.2',
+    's',
+    'C',
+    1
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    3,
+    'single question 1.3',
+    's',
+    'B',
+    1
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    4,
+    'single question 1.4',
+    's',
+    'A',
+    1
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    5,
+    'multiple question 1.1',
+    'm',
+    'AC',
+    1
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    6,
+    'multiple question 1.2',
+    'm',
+    'ABC',
+    1
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    7,
+    'multiple question 1.3',
+    'm',
+    'BCD',
+    1
+);
+
+-- add for virus 2
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    8,
+    'single question 2.1',
+    's',
+    'A',
+    2
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    9,
+    'single question 2.2',
+    's',
+    'B',
+    2
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    10,
+    'single question 2.3',
+    's',
+    'A',
+    2
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    11,
+    'single question 2.4',
+    's',
+    'B',
+    2
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    12,
+    'multiple question 2.1',
+    'm',
+    'ABC',
+    2
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    13,
+    'multiple question 2.2',
+    'm',
+    'BC',
+    2
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    14,
+    'multiple question 2.3',
+    'm',
+    'CD',
+    2
+);
+
+-- add for virus 3
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    15,
+    'single question 3.1',
+    's',
+    'C',
+    3
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    16,
+    'single question 3.2',
+    's',
+    'C',
+    3
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    17,
+    'single question 3.3',
+    's',
+    'A',
+    3
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    18,
+    'single question 3.4',
+    's',
+    'C',
+    3
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    19,
+    'multiple question 3.1',
+    'm',
+    'BC',
+    3
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    20,
+    'multiple question 3.2',
+    'm',
+    'AD',
+    3
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    21,
+    'multiple question 3.3',
+    'm',
+    'ABCD',
+    3
 );
