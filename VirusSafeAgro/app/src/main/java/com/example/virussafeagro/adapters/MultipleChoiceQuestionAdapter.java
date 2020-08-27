990269415c8140945a4736b8097da9963f5ba156
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-27 15:19:58 +0800

    Add MultipleChoiceQuestionAdapter

---
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/adapters/MultipleChoiceQuestionAdapter.java
@@ -0,0 +1,61 @@
+package com.example.virussafeagro.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.virussafeagro.R;
+import com.example.virussafeagro.models.MultipleChoiceQuestionModel;
+
+import java.util.List;
+
+public class MultipleChoiceQuestionAdapter extends RecyclerView.Adapter<MultipleChoiceQuestionAdapter.ViewHolder>{
+    private List<MultipleChoiceQuestionModel> multipleChoiceQuestionModelList;
+    private FragmentActivity fragmentActivity;
+
+    public MultipleChoiceQuestionAdapter(List<MultipleChoiceQuestionModel> multipleChoiceQuestionModelList, FragmentActivity fragmentActivity) {
+        this.multipleChoiceQuestionModelList = multipleChoiceQuestionModelList;
+        this.fragmentActivity = fragmentActivity;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView multipleChoiceQuestionContentTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            this.multipleChoiceQuestionContentTextView = itemView.findViewById(R.id.tv_multiple_question_content_virus_quiz_question);
+        }
+    }
+
+    @NonNull
+    @Override
+    public MultipleChoiceQuestionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View multipleChoiceQuestionView = inflater.inflate(R.layout.recycler_view_multiple_choice_question, parent, false);
+        MultipleChoiceQuestionAdapter.ViewHolder viewHolder = new MultipleChoiceQuestionAdapter.ViewHolder(multipleChoiceQuestionView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MultipleChoiceQuestionAdapter.ViewHolder viewHolder, int position) {
+        final MultipleChoiceQuestionModel multipleChoiceQuestionModel = this.multipleChoiceQuestionModelList.get(position);
+
+        viewHolder.multipleChoiceQuestionContentTextView.setText(multipleChoiceQuestionModel.getMultipleChoiceQuestionContent());
+
+        // bind view holder for multiple-question options
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.multipleChoiceQuestionModelList.size();
+    }
+}
--- VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/VirusQuizQuestionFragment.java
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/VirusQuizQuestionFragment.java
@@ -4,29 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.virussafeagro.R;
+import com.example.virussafeagro.adapters.SingleChoiceQuestionAdapter;
+import com.example.virussafeagro.models.MultipleChoiceQuestionModel;
+import com.example.virussafeagro.models.SingleChoiceQuestionModel;
 import com.example.virussafeagro.models.VirusModel;
+
+import java.util.List;
 
 public class VirusQuizQuestionFragment extends Fragment {
     private View view;
     private VirusModel currentVirusModel;
+
+    private List<SingleChoiceQuestionModel> singleChoiceQuestionModelList;
+    private List<MultipleChoiceQuestionModel> multipleChoiceQuestionModelList;
+
+    private TextView virusFullNameTitleTextView;
+    private Button submitAnswerButton;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the View for this fragment
         this.view = inflater.inflate(R.layout.fragment_virus_quiz_question, container, false);
-        this.initializeViews();
 
         // get passed bundle and the VirusModel within it
         Bundle bundle = getArguments();
         assert bundle != null;
         this.currentVirusModel = bundle.getParcelable("currentVirusModel");
+        this.initializeViews();
 
         return this.view;
     }
@@ -35,14 +52,20 @@ public class VirusQuizQuestionFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        showVirusQuizQuestions();
     }
 
+    // bind virus full name to the title
     private void initializeViews() {
-
+        this.virusFullNameTitleTextView = view.findViewById(R.id.tv_title_virus_full_name_quiz_question);
+        this.virusFullNameTitleTextView.setText(this.currentVirusModel.getVirusFullName());
     }
 
-    private void showVirusQuizQuestions() {
-
+    private void showVirusQuizSingleChoiceQuestions() {
+        SingleChoiceQuestionAdapter singleChoiceQuestionAdapter = new SingleChoiceQuestionAdapter(singleChoiceQuestionModelList, requireActivity());
+        RecyclerView recyclerViewForSingleChoiceQuestionModelList = view.findViewById(R.id.rv_virus_single_choice_quiz_question);
+        recyclerViewForSingleChoiceQuestionModelList.addItemDecoration(new DividerItemDecoration(requireActivity(), LinearLayoutManager.VERTICAL));
+        recyclerViewForSingleChoiceQuestionModelList.setAdapter(singleChoiceQuestionAdapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(requireActivity());
+        recyclerViewForSingleChoiceQuestionModelList.setLayoutManager(layoutManager);
     }
 }
