37b680b6522156c3ca7ca96a810243948646c1aa
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-28 10:37:46 +0800

    Modify Sql script of adding option records for testing

--- SQL script/Test-add opiton record.sql
+++ SQL script/Test-add opiton record.sql
@@ -1,8 +1,675 @@
--- Add for virus 1
-INSERT INTO choiceoption (choice_opiton_id, choice_opiton_label, choice_opiton_content, choice_opiton_question_id) VALUES 
+-- add for virus 1
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
 (
     1,
-    'A',
-    'option 1',
+    'a',
+    's option 1',
     1
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    2,
+    'b',
+    's option 2',
+    1
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    3,
+    'c',
+    's option 3',
+    1
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    4,
+    'd',
+    's option 4',
+    1
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    5,
+    'a',
+    's option 1',
+    2
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    6,
+    'b',
+    's option 2',
+    2
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    7,
+    'c',
+    's option 3',
+    2
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    8,
+    'a',
+    's option 1',
+    3
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    9,
+    'b',
+    's option 2',
+    3
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    10,
+    'c',
+    's option 3',
+    3
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    11,
+    'a',
+    's option 1',
+    4
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    12,
+    'b',
+    's option 2',
+    4
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    13,
+    'c',
+    's option 3',
+    4
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    14,
+    'd',
+    's option 4',
+    4
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    15,
+    'a',
+    'm option 1',
+    5
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    16,
+    'b',
+    'm option 2',
+    5
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    17,
+    'c',
+    'm option 3',
+    5
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    18,
+    'd',
+    'm option 4',
+    5
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    19,
+    'a',
+    'm option 1',
+    6
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    20,
+    'b',
+    'm option 2',
+    6
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    21,
+    'c',
+    'm option 3',
+    6
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    22,
+    'd',
+    'm option 4',
+    6
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    23,
+    'e',
+    'm option 5',
+    6
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    24,
+    'a',
+    'm option 1',
+    7
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    25,
+    'b',
+    'm option 2',
+    7
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    26,
+    'c',
+    'm option 3',
+    7
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    27,
+    'd',
+    'm option 4',
+    7
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    28,
+    'e',
+    'm option 5',
+    7
+);
+
+-- add for virus 2
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    29,
+    'a',
+    's option 1',
+    8
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    30,
+    'b',
+    's option 2',
+    8
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    31,
+    'c',
+    's option 3',
+    8
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    32,
+    'd',
+    's option 4',
+    8
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    33,
+    'a',
+    's option 1',
+    9
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    34,
+    'b',
+    's option 2',
+    9
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    35,
+    'c',
+    's option 3',
+    9
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    36,
+    'a',
+    's option 1',
+    10
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    37,
+    'b',
+    's option 2',
+    10
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    38,
+    'c',
+    's option 3',
+    10
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    39,
+    'a',
+    's option 1',
+    11
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    40,
+    'b',
+    's option 2',
+    11
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    41,
+    'c',
+    's option 3',
+    11
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    42,
+    'd',
+    's option 4',
+    11
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    43,
+    'a',
+    'm option 1',
+    12
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    44,
+    'b',
+    'm option 2',
+    12
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    45,
+    'c',
+    'm option 3',
+    12
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    46,
+    'd',
+    'm option 4',
+    12
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    47,
+    'a',
+    'm option 1',
+    13
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    48,
+    'b',
+    'm option 2',
+    13
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    49,
+    'c',
+    'm option 3',
+    13
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    50,
+    'd',
+    'm option 4',
+    13
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    51,
+    'e',
+    'm option 5',
+    13
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    52,
+    'a',
+    'm option 1',
+    14
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    53,
+    'b',
+    'm option 2',
+    14
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    54,
+    'c',
+    'm option 3',
+    14
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    55,
+    'd',
+    'm option 4',
+    14
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    56,
+    'e',
+    'm option 5',
+    14
+);
+
+-- add for virus 3
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    57,
+    'a',
+    's option 1',
+    15
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    58,
+    'b',
+    's option 2',
+    15
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    59,
+    'c',
+    's option 3',
+    15
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    60,
+    'd',
+    's option 4',
+    15
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    61,
+    'a',
+    's option 1',
+    16
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    62,
+    'b',
+    's option 2',
+    16
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    63,
+    'c',
+    's option 3',
+    16
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    64,
+    'a',
+    's option 1',
+    17
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    65,
+    'b',
+    's option 2',
+    17
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    66,
+    'c',
+    's option 3',
+    17
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    67,
+    'a',
+    's option 1',
+    18
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    68,
+    'b',
+    's option 2',
+    18
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    69,
+    'c',
+    's option 3',
+    18
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    70,
+    'd',
+    's option 4',
+    18
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    71,
+    'a',
+    'm option 1',
+    19
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    72,
+    'b',
+    'm option 2',
+    19
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    73,
+    'c',
+    'm option 3',
+    19
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    74,
+    'd',
+    'm option 4',
+    19
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    75,
+    'a',
+    'm option 1',
+    20
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    76,
+    'b',
+    'm option 2',
+    20
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    77,
+    'c',
+    'm option 3',
+    20
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    78,
+    'd',
+    'm option 4',
+    20
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    79,
+    'e',
+    'm option 5',
+    20
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    80,
+    'a',
+    'm option 1',
+    21
+);
+
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    81,
+    'b',
+    'm option 2',
+    21
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    82,
+    'c',
+    'm option 3',
+    21
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    83,
+    'd',
+    'm option 4',
+    21
+);
+
+INSERT INTO choiceoption (choice_option_id, choice_option_label, choice_option_content, choice_option_question_id) VaLUES 
+(
+    84,
+    'e',
+    'm option 5',
+    21
 );
