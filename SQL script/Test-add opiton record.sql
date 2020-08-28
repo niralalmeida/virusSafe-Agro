34c54a9699468b8acd9d748efcf1a8297dde21bf
Haoyu Yang <hyan0050@student.monash.edu>
2020-08-28 09:40:16 +0800

    Add Sql script of adding option records for testing

---
+++ SQL script/Test-add opiton record.sql
@@ -0,0 +1,8 @@
+-- Add for virus 1
+INSERT INTO choiceoption (choice_opiton_id, choice_opiton_label, choice_opiton_content, choice_opiton_question_id) VALUES 
+(
+    1,
+    'A',
+    'option 1',
+    1
+);
