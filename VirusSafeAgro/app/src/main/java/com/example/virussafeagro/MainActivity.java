d79e8d68a6c604f69e89fbbbe7ec6bbe4547bd9a
Haoyu Yang <hyan0050@student.monash.edu>
2020-09-03 18:01:50 +0800

    Add Home Fragment

--- VirusSafeAgro/app/src/main/java/com/example/virussafeagro/MainActivity.java
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.example.virussafeagro.fragments.HomeFragment;
 import com.example.virussafeagro.fragments.VirusCheckFragment;
 import com.example.virussafeagro.fragments.VirusInfoListFragment;
 import com.example.virussafeagro.fragments.VirusQuizListFragment;
@@ -29,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         // show or not top action bar (back button + title)
         showTopActionBar(this);
 
+        // show home fragment
+        FragmentOperator.replaceFragmentNoBackStack(this, new HomeFragment());
+
         // initialize bottom navigation bar
         this.initializeBottomNavigationView();
     }
@@ -36,13 +40,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     // show or not top action bar
     public static void showTopActionBar(MainActivity mainActivity) {
         FragmentManager fragmentManager = mainActivity.getSupportFragmentManager();
-//        Fragment currentVisibleFragment = FragmentOperator.getVisibleFragment(fragmentManager);
         Fragment currentVisibleFragment = fragmentManager.findFragmentById(R.id.fl_fragments);
 
+        boolean isHomeFragment = currentVisibleFragment instanceof HomeFragment;
         boolean isVirusInfoListFragment = currentVisibleFragment instanceof VirusInfoListFragment;
         boolean isVirusIdentificationFragment = currentVisibleFragment instanceof VirusCheckFragment;
         boolean isVirusQuizListFragment = currentVisibleFragment instanceof VirusQuizListFragment;
-        if ((currentVisibleFragment == null) || isVirusInfoListFragment || isVirusIdentificationFragment || isVirusQuizListFragment){
+        if ((currentVisibleFragment == null) || isHomeFragment || isVirusInfoListFragment || isVirusIdentificationFragment || isVirusQuizListFragment){
             Objects.requireNonNull(mainActivity.getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
             Objects.requireNonNull(mainActivity.getSupportActionBar()).setHomeButtonEnabled(false);
         } else {
@@ -52,11 +56,11 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         }
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        FragmentOperator.backToLastFragment(this);
-    }
+//    @Override
+//    public void onBackPressed() {
+//        super.onBackPressed();
+//        FragmentOperator.backToLastFragment(this);
+//    }
 
     // initialize BottomNavigationView and set OnNavigationItemSelectedListener
     private void initializeBottomNavigationView(){
@@ -67,7 +71,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        this.hideHomePagePicture();
 
         // open fragment according to id
         this.switchFragments(id);
@@ -77,6 +80,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     private void switchFragments(int itemId) {
         switch (itemId) {
+            case R.id.ic_home:
+                FragmentOperator.replaceFragmentNoBackStack(this, new HomeFragment());
+                break;
             case R.id.ic_virus_info:
                 FragmentOperator.replaceFragmentNoBackStack(this, new VirusInfoListFragment());
                 break;
@@ -87,10 +93,5 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 FragmentOperator.replaceFragmentNoBackStack(this, new VirusQuizListFragment());
                 break;
         }
-    }
-
-    private void hideHomePagePicture(){
-        LinearLayout homeLinearLayout = findViewById(R.id.ll_home_picture);
-        homeLinearLayout.setVisibility(View.INVISIBLE);
     }
 }
