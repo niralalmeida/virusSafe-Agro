59fe72770b2bff06240a0ca3a73388247fe4bb03
Niral Almeida <44018270+niralalmeida@users.noreply.github.com>
2020-09-03 23:05:10 +1000

    Update Test-add question record.sql

--- SQL script/Test-add question record.sql
+++ SQL script/Test-add question record.sql
@@ -1,191 +1,154 @@
--- add for virus 1
+
+------------------------------------------VIRUS 1 QUESTIONS--------------------------------------------
+--VIRUS 1: QUESTION 1--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     1,
-    'single question 1.1',
+    'Are there chemical control strategies available to control the spread of tomato Mosaic Virus (ToMV)?',
     's',
     'B',
     1
 );
 
+--VIRUS 1: QUESTION 2--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     2,
-    'single question 1.2',
-    's',
-    'C',
+    'What are the common symptoms observed in tomato (fruit)?',
+    'm',
+    'ABC',
     1
 );
 
+--VIRUS 1: QUESTION 3--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     3,
-    'single question 1.3',
-    's',
-    'B',
-    1
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    4,
-    'single question 1.4',
+    'Which disinfectant should be used to sterilise tools after handling infected plants?',
     's',
     'A',
     1
 );
 
+--VIRUS 1: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    4,
+    'What are the means of infection for Tomato Mosaic Virus (ToMV)?',
+    's',
+    'D',
+    1
+);
+
+--VIRUS 1: QUESTION 5--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     5,
-    'multiple question 1.1',
-    'm',
-    'AC',
+    'Which of the following environmental conditions influence  the symptoms of Tomato Mosaic Virus (ToMV)?',
+    's',
+    'D',
     1
 );
+
+------------------------------------------VIRUS 2 QUESTIONS--------------------------------------------
+--VIRUS 2: QUESTION 1--
 
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     6,
-    'multiple question 1.2',
+    'Which of the following chemical products can be used to control spread target spots in tomato plants?',
     'm',
     'ABC',
     1
 );
 
+--VIRUS 2: QUESTION 2--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     7,
-    'multiple question 1.3',
+    'What are the common symptoms observed in tomato (fruit)?',
     'm',
-    'BCD',
+    'AB',
     1
 );
 
--- add for virus 2
+--VIRUS 2: QUESTION 3--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     8,
-    'single question 2.1',
+    'The target spots grow up to:',
     's',
     'A',
     2
 );
 
+--VIRUS 2: QUESTION 4--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     9,
-    'single question 2.2',
+    'What are the means of infection for target spots in tomato plants?',
     's',
-    'B',
+    'C',
     2
 );
 
+--VIRUS 2: QUESTION 5--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     10,
-    'single question 2.3',
-    's',
-    'A',
+    'Target spot attacks which area of the plant first?',
+    'm',
+    'AB',
     2
 );
 
+------------------------------------------VIRUS 3 QUESTIONS--------------------------------------------
+--VIRUS 3: QUESTION 1--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     11,
-    'single question 2.4',
-    's',
-    'B',
-    2
+    'Which of the following chemical products can be used to control spread bacterial spots in tomato plants ?',
+    'm',
+    'AB',
+    3
 );
 
+--VIRUS 3: QUESTION 2--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
     12,
-    'multiple question 2.1',
+    'What are the common symptoms observed in tomato (fruit)?',
     'm',
-    'ABC',
-    2
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    13,
-    'multiple question 2.2',
-    'm',
-    'BC',
-    2
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    14,
-    'multiple question 2.3',
-    'm',
-    'CD',
-    2
-);
-
--- add for virus 3
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    15,
-    'single question 3.1',
-    's',
-    'C',
-    3
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    16,
-    'single question 3.2',
-    's',
-    'C',
-    3
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    17,
-    'single question 3.3',
-    's',
     'A',
     3
 );
 
+--VIRUS 3: QUESTION 3--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
-    18,
-    'single question 3.4',
+    13,
+    'How wide are the bacterial spots on the tomato leaves?',
+    'm',
+    'A',
+    3
+);
+
+--VIRUS 3: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    14,
+    'What are the environmental conditions that influence the spread of bacterial spots?',
     's',
-    'C',
+    'E',
     3
 );
 
+--VIRUS 3: QUESTION 5--
 INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
 (
-    19,
-    'multiple question 3.1',
-    'm',
-    'BC',
-    3
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    20,
-    'multiple question 3.2',
-    'm',
-    'AD',
-    3
-);
-
-INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
-(
-    21,
-    'multiple question 3.3',
-    'm',
-    'ABCD',
+    15,
+    'Bacterial spots affect which of the following parts of the plant?',
+    's',
+    'E',
     3
 );
