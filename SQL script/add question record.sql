c2e1ae66a44a842fc1416714b0ed78671fc3468a
Haoyu Yang <hyan0050@student.monash.edu>
2020-09-04 09:20:19 +0800

    Modify the SQL script for adding records

---
+++ SQL script/add question record.sql
@@ -0,0 +1,462 @@
+
+-- VIRUS 1: QUESTION 1--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    1,
+    'Are there chemical control strategies available to control the spread of tomato Mosaic Virus (ToMV)?',
+    's',
+    'B',
+    1
+);
+
+-- VIRUS 1: QUESTION 2--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    2,
+    'What are the common symptoms observed in tomato (fruit)?',
+    'm',
+    'ABC',
+    1
+);
+
+-- VIRUS 1: QUESTION 3--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    3,
+    'Which disinfectant should be used to sterilise tools after handling infected plants?',
+    's',
+    'A',
+    1
+);
+
+-- VIRUS 1: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    4,
+    'What are the means of infection for Tomato Mosaic Virus (ToMV)?',
+    's',
+    'D',
+    1
+);
+
+-- VIRUS 1: QUESTION 5--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    5,
+    'Which of the following environmental conditions influence  the symptoms of Tomato Mosaic Virus (ToMV)?',
+    's',
+    'D',
+    1
+);
+
+
+-- VIRUS 2: QUESTION 1--
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    6,
+    'Which of the following chemical products can be used to control spread target spots in tomato plants?',
+    'm',
+    'ABC',
+    2
+);
+
+-- VIRUS 2: QUESTION 2--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    7,
+    'What are the common symptoms observed in tomato (fruit)?',
+    'm',
+    'AB',
+    2
+);
+
+-- VIRUS 2: QUESTION 3--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    8,
+    'The target spots grow up to:',
+    's',
+    'A',
+    2
+);
+
+-- VIRUS 2: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    9,
+    'What are the means of infection for target spots in tomato plants?',
+    's',
+    'C',
+    2
+);
+
+-- VIRUS 2: QUESTION 5--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    10,
+    'Target spot attacks which area of the plant first?',
+    'm',
+    'AB',
+    2
+);
+
+
+-- VIRUS 3: QUESTION 1--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    11,
+    'Which of the following chemical products can be used to control spread bacterial spots in tomato plants ?',
+    'm',
+    'AB',
+    3
+);
+
+-- VIRUS 3: QUESTION 2--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    12,
+    'What are the common symptoms observed in tomato (fruit)?',
+    'm',
+    'A',
+    3
+);
+
+-- VIRUS 3: QUESTION 3--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    13,
+    'How wide are the bacterial spots on the tomato leaves?',
+    'm',
+    'A',
+    3
+);
+
+-- VIRUS 3: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    14,
+    'What are the environmental conditions that influence the spread of bacterial spots?',
+    's',
+    'E',
+    3
+);
+
+-- VIRUS 3: QUESTION 5--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    15,
+    'Bacterial spots affect which of the following parts of the plant?',
+    's',
+    'E',
+    3
+);
+
+
+
+-- add for virus 4
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    16,
+    'The genome of tomato yellow curl virus is made up of:',
+    's',
+    'A',
+    4
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    17,
+    'What are the common symptoms observed in tomato (fruit)?',
+    's',
+    'C',
+    4
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    18,
+    'Tomato yellow curl virus is spread by which fly?',
+    's',
+    'A',
+    4
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    19,
+    'Tomato yellow leaf curl virus is not transmitted in seed and through soil',
+    's',
+    'A',
+    4
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    20,
+    'What are the common symptoms observed of yellow leaf curl virus in tomato leaves?',
+    's',
+    'E',
+    4
+);
+
+
+
+
+
+-- add for virus 5
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    21,
+    'TWhat are the common symptoms of late blight observed in tomato (fruit)?',
+    's',
+    'B',
+    5
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    22,
+    'Preventive sprays should not be used before symptoms are seen',
+    's',
+    'B',
+    5
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    23,
+    'Tomato late blight is Airborne',
+    's',
+    'A',
+    5
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    24,
+    'Tomato late blight is favoured by wet and cool weather',
+    's',
+    'A',
+    5
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    25,
+    'Which of the following preventive sprays can be used to prevent the spread of the virus?',
+    's',
+    'D',
+    5
+);
+
+
+
+
+
+-- add for virus 6
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    26,
+    'n tomato leaf mold virus, pale green or yellowish spots are present on:',
+    's',
+    'A',
+    6
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    27,
+    'Leaf mold is a serious issue in on- field grown tomatoes:',
+    's',
+    'B',
+    6
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    28,
+    'In tomato leaf mold virus, Olive, green mold is visible on:',
+    's',
+    'B',
+    6
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    29,
+    'Conidia can be easily spread by rain and wind',
+    's',
+    'A',
+    6
+);
+
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    30,
+    'The leaves should be sprayed at an 10-14 day intervals, when yellow spots are seen',
+    's',
+    'A',
+    6
+);
+
+
+
+
+
+
+
+
+
+-- VIRUS 7: QUESTION 1--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    31,
+    'In early blight, spots grow up to half inch in diameter?',
+    's',
+    'A',
+    7
+);
+
+-- VIRUS 7: QUESTION 2--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    32,
+    'What are the common symptoms observed in tomato (fruit)?',
+    's',
+    'D',
+    7
+);
+
+-- VIRUS 7: QUESTION 3--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    33,
+    'Early blight can occur in any type of weather?',
+    's',
+    'A',
+    7
+);
+
+-- VIRUS 7: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    34,
+    'Early blight commonly affects?',
+    's',
+    'D',
+    7
+);
+
+-- VIRUS 7: QUESTION 5--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    35,
+    'Early blight Strikes after a period of heavy rainfal?',
+    's',
+    'A',
+    7
+);
+
+
+
+-- VIRUS 8: QUESTION 1--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    36,
+    'Miticide spray should be applied if mites are present on at least 10% of the leaves?',
+    's',
+    'B',
+    8
+);
+
+-- VIRUS 8: QUESTION 2--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    37,
+    '. One effective application of chemical is sufficient?',
+    's',
+    'A',
+    8
+);
+
+-- VIRUS 8: QUESTION 3--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    38,
+    ' Adult spider mites are 0.8 mm in size?',
+    's',
+    'B',
+    8
+);
+
+-- VIRUS 8: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    39,
+    'Insecticides should be applied even if there are no mites present on leaves?',
+    's',
+    'B',
+    8
+);
+
+-- VIRUS 8: QUESTION 5--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    40,
+    'Spraying chemicals to plants should be avoided in hot weathers as they may cause chemical injuries to plants?',
+    's',
+    'A',
+    8
+);
+
+
+-- VIRUS 9: QUESTION 1--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    41,
+    'Fungicidal spray will protect infected leaves?',
+    's',
+    'B',
+    9
+);
+
+-- VIRUS 9: QUESTION 2--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    42,
+    'Which are the most commonly used chemicals to treat plants from Septoria blight',
+    'm',
+    'CD',
+    9
+);
+
+-- VIRUS 9: QUESTION 3--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    43,
+    'For effective results the chemicals must be applied at 7-10-day intervals?',
+    's',
+    'A',
+    9
+);
+
+-- VIRUS 9: QUESTION 4--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    44,
+    'Damp conditions are favourable for virus to develop?',
+    's',
+    'A',
+    9
+);
+
+-- VIRUS 9: QUESTION 5--
+INSERT INTO choicequestion (choice_question_id, choice_question_content, choice_question_type, answer, choice_question_virus_id) VALUES 
+(
+    45,
+    'Diameter of Septoria leaf spot is about?',
+    's',
+    'A',
+    9
+);
---
+++ SQL script/add_virus_record.sql
@@ -0,0 +1,142 @@
+
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    1,
+    'TOMATO MOSAIC VIRUS ',
+    'ToMV',
+    '• Belongs to the family of Virgaviridae\n • Is a plant pathogenic virus\n • Commonly found in tomatoes and many other plants',
+    '1. LEAVES:\n •	Light green mosaic patterns on the leaves\n • Alternating yellowish and darker green areas\n 2. APPEARANCE:\n Leaves appear to be fern like in appearance and tend to have pointed tips\n 3. FRUIT:\n •	Bronze and patchy appearance develops on the fruit\n •	Distorted, yellow blotches, necrotic spots on fruit\n',
+    '1. TIMING OF CAUSE:\n Any time during the growing season\n 2. ENVIRONMENTAL CONDITIONS:\n•	Environmental conditions influence the symptoms\n •	Influencing factors:\n Temperature, day length, intensity of light, variety, plant age at the time of infection\n 3. MEANS OF INFECTION:\n • Infected seed\n • Small number of infected seedlings are enough for virus to spread rapidly\n • Contaminated tools, hands and clothes of workers\n',
+    ' ',
+    '1.	NON-CHEMICAL CONTROL:\n •Remove plants with symptoms Promptly to prevent the spread of the virus to nearby healthy plants\n • On handling infected plants, it’s necessary to wash tools and hands in hot soapy water\n • Tools should be sterilised using a disinfectant (Virkon S)\n •	Avoid growing new plants in close proximity if you suspect your plants are infected\n 2. CHEMICAL CONTROL:\n No chemical controls available',
+    ' '
+);
+
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    2,
+    'TARGET SPOT',
+    'TSP',
+    '• Target spot attacks leaves or stems of tomato plants\n •Also called as leaf spot\n •Scientific Name: Corynespora cassiicola\n •Distribution: Widespread\n',
+    '• Brown and black spots appear on leaves, which keeps on spreading\n •	Disease starts on the older leaves\n • Spread of virus is rapid, and the plant loses its leaves quickly\n 2. APPEARANCE: \n• The spots are oval to angular in shape\n •	They form series of concentric circles\n • The spots grow up to 10-12 mm\n 3. FRUIT:\n • Difficult to identify target spot on tomato fruit in early stage\n • Circular spots with velvety black fungal lesion are observed after the diseased tomato ripens\n',
+    '1. TIMING OF CAUSE: Any time during the growing season\n 2. ENVIRONMENTAL CONDITIONS:\n •	Moderate temperatures\n • High moisture\n 3. MEANS OF INFECTION:\n • Infected seed\n • Remains of earlier infected plants or host species\n',
+    ' ',
+    '1.	NON-CHEMICAL CONTROL:\n • Remove debris of old plants at the end of growing season\n • Avoid planting in the areas where diseased plants where located in the past years\n • Plants should be grown in sunlight with proper air circulation\n •	Avoid crowding of plants\n • Remove branches from lower part of the plants for better airflow\n 2. CHEMICAL CONTROL:\n • Chemical products used:\n o Mancozeb\n o copper oxychloride\n o chlorothalonil\n • Plants should be treated when the first spots are seen and should be sprayed at 10-14 days interval for three-four weeks\n • Should be sprayed on both sides of the leaves\n',
+    ' '
+);
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    3,
+    'BACTERIAL SPOT',
+    'BST',
+    '• Bacterial spot is considered as one of the devastating diseases\n • It’s impossible to control the disease once present in the plant\n •	Scientific name: Xanthomonas vesicatoria\n • Affects both above and ground parts of tomato plants: fruits, stems, leaves, and petioles\n',
+    '1. LEAVES:\n •	Leaves have small (<1/8 inch), brown, circular spots encircling a yellow halo\n • Sunken upper leaf surface\n 2. APPEARANCE: Spots are about 3 mm and usually found on tips and margins of leaves\n 3. FRUIT:\n • Fruit spots are ¼ in inch\n • Slightly raised, scabby, and brown\n • Fruit has a waxy white halo surrounding\n',
+    '1.	TIMING OF CAUSE:\n Any time during the growing season\n 2. ENVIRONMENTAL CONDITIONS:\n • High temperatures\n• High humidity\n •	Overhead irrigation\n •	Frequent rainfall\n 3. MEANS OF INFECTION:\n • Infected seed\n',
+    ' ',
+    '1.	NON-CHEMICAL CONTROL:\n • Grow plants from infection free seeds\n •	Purchase high quality seeds\n •	Use hot water treatment to kill bacteria in seeds\n • Avoid high pressure sprays\n • Rotate between crops\n • Remove crop debris at the end of the growing season\n • Disinfect pruners and tools by dipping in commercial sanitizer\n 2. CHEMICAL CONTROL:\n• Copper fungicides or\n •	Copper plus mancozeb\n • Chemicals should be sprayed at 7-10 days interval in the field\n •	Spray early when plants are young',
+    ' '
+);
+
+-- add for virus 4
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    4,
+    'TOMATO YELLOW LEAF CURL VIRUS',
+    'TYLC',
+    'DESCRIPTION:\n • It is a DNA virus and belongs to the family of Geminiviridae\n • Is one of the most damaging pathogens of tomato plants\n • It is often spread by whieflies',
+    ' 1. LEAVES:\n • Leaves are often bent downwards, thick, and stiff\n • Have leathery texture and are wrinkled\n 2. APPEARANCE\n • Plants are severely stunted and will not develop fruits\n • Small leaves become yellow between the veins\n • Flowers usually fall off or do not develop at all\n 3. FRUIT:\n • Will not produce fruit\n • If at all the fruit is produced, it is small, dry, and unsaleable\n ',
+    'CAUSE:\n • MEANS OF INFECTION:\n • It is not transmitted in seed nor through soil\n •It is harboured in infected host plants\n •TYLCV is spready by silverleaf whitefly insect\n
+    • The whiteflies carry the virus by feeding on the infected host plants\n • The whiteflies spread the virus to the healthy plants, which shows the symptoms 10-21 days later',
+    '',
+    'PREVENTION:\n 1. NON-CHEMICAL CONTROL:\n • Manage flies\n • Remove old crop debris and destroy them\n 2. CHEMICAL CONTROL:\n • Silverleaf whiteflies can be bought under 
+    control using potent chemicals',
+    ''
+);
+
+-- add for virus 5
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    5,
+    'TOMATO LATE BLIGHT',
+    'TOLB',
+    'DESCRIPTION:\n • Late blight is considered as one of the most serious fungal diseases in tomato plants\n • Scientific name: Phytophthora
+    infestans\n • Impact: Cause large losses in yields of tomato',
+    'SYMPTOMS:\n 1. LEAVES:\n • Black or brown irregular shape spots, which grow rapidly forming a white cottony growth\n 2. APPEARANCE:\n
+     • Rapid growth of spots and patches on leaves with a furry white growth on the underside\n
+     • Water soaked appearance in leaf, stem, and fruit lesions\n 
+     3. FRUIT:\n • Dark brown, firm, rots appear on the tomato fruit',
+    'CAUSE:\n MEANS OF INFECTION:\n • Infected transplants\n • Airborne\n • Virus travel great distances in storms\n • Favoured by cool and wet weather',
+    '',
+    'PREVENTION:\n 1. NON-CHEMICAL CONTROL:\n • Rotate crops, avoid planting tomato plants on same land\n 2. CHEMICAL CONTROL:\n • Expensive fungicides should
+    be used to maintain control\n • Spray plants with fungicides if unsure about the symptoms\n Preventive sprays should be used before symptoms are seen\n 
+    • Preventive sprays:\n <1> Copper product\n <2> Chlorothalonil\n <3> Mancozeb\n • Systemic products such as strobilurin, dimethomorth, cymoxanil,and metalaxyl',
+    ''
+);
+
+-- add for virus 6
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    6,
+    'LEAF MOLD',
+    'TOLM',
+    'DESCRIPTION:\n • Leaf mold is caused by Passalora fulva\n • It is found worldwide and occasionally found in on field grown tomatoes\n 
+    • It is a serious issue in green house grown tomatoes\n',
+    'SYMPTOMS:\n 1. LEAVES:\n • Pale green or yellowish spots on the upper leaf surface\n • Olive, green mold is visible on the lower leaf surface\n 
+     2. APPEARANCE:\n •Yellow spots\n • Irregular patches\n • Dried leaves\n 3. FRUIT:\n • Dark,leathery, rot at the stem ends are the common
+     symptoms seen on fruits\n',
+    'CAUSE:\n MEANS OF INFECTION:\n • Contaminated seed serves as an initial source of spread\n • Passalora fulva survives as saprophyte on
+    infected plant debris, as conidia\n • Conidia can be easily spread by rain and wind\n • Conidia can also be spread on tools and workers clothes\n',
+    '',
+    'PREVENTION:\n 1.NON-CHEMICAL CONTROL:\n • Remove infected lower leaves\n • Burn or dig remains of infected plants\n • Pruning to increase ventilation\n
+     2. CHEMICAL CONTROL:\n • Use fungicides to give adequate control\n • Chemical products which can be used:\n <1> Mancozeb\n <2> Copper 
+     oxychloride\n <3> Chlorothalonil\n • Treatment should start when yellow spots are seen\n • The leaves should be sprayed at an 10-14 day
+     intervals',
+    ''
+);
+
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    7,
+    'EARLY BLIGHT',
+    'EB',
+    '• Most common tomato disease \n • Occurs nearly every season \n • Usually affects leaves, stems and fruits \n • It is caused by fungus named Alternaria solani',
+    '1. LEAVES: \n • Small dark spots are observed near the ground of the plant \n • Round and brown leaf spots, which grows up to half inch in diameter \n • Infected leaves turn brown and fall off \n 2. APPEARANCE: \n  • Dry brown areas with dark brown concentric circles \n 3. FRUIT: \n  • Leathery and black spots on fruits \n • Infected fruit often falls off the plant',
+    '1. TIMING OF CAUSE: \n  • Evident during early to mid-season \n  2. ENVIRONMENTAL CONDITIONS: \n  • Can occur in any type of weather \n • Favours damp conditions like frequent rains or heavy dews \n • Strikes after a period of heavy rainfall \n  • Disease develop atmoderate to warm temperatures \n  3. MEANS OF INFECTION: \n  • Infected seedlings \n • Diseased debris',
+    '',
+    '1. NON-CHEMICAL CONTROL: \n • Use pathogen free seed \n • Blight remains active for a year, so it isrecommended to rotate crops \n • Avoid overcrowding of plants so that air can pass through the leaves and keep them dry \n • Water the spoil and not the plants \n • Destroy infected plants \n  2. CHEMICAL CONTROL: \n  • Use effective fungicides \n  • Fungicides include: \n  o Mancozeb \n  o Maneb \n  o Chlorothalonil \n  o Copper (Kocide) \n  o Penthiopyrad  \n o Boscalid \n o Pyraclostrobin',
+    ''
+);
+
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    8,
+    'TWO-SPOTTED SPIDER MITE',
+    'TSSM',
+    '• Most common mite species that attacks vegetables especially the tomatoes \n  • Adult spider mites are less than 0.5 mm long with eight legs \n • Scientific name: Tetranychus urticae \n  • Damage caused by the mites is infrequent and minor \n • The mites suck individual plant cells',
+    '1. LEAVES: \n • Yellow mottled or stippled areas can be found on underside of leaves \n • Damage can first be observed at the leaf veins \n • Distorted leaves \n  • Whitening or spotting of leaves \n 2. APPEARANCE: \n  • The feeding of mites on plant cells leads to extensive damage to leaves, fruits and flower \n • Leaves turn yellow and drop',
+    'ENVIRONMENTAL CONDITIONS: \n  • Mites are favoured by hot dry weather',
+    '',
+    '1. NON-CHEMICAL CONTROL: \n • Avoid weedy fields \n • Avoid over fertilising \n 2. CHEMICAL CONTROL: \n • Miticide spray should be applied if mites are present on 50% of the leaves \n  • Insecticides should be applied only as needed \n  • Usage of pesticides is recommended, which will have minimal impact on useful insects \n • Chemical application should be done at the undersides of the leaves \n  • One effective application of chemical is sufficient \n • Spraying chemicals to plants should be  avoided in hot weathers as they may cause chemical injuries ',
+    ''
+);
+
+INSERT INTO virus (virus_id, virus_full_name, virus_abbreviation, virus_description, symptoms, causes, spread, prevention, virus_distribution) VALUES 
+(
+    9,
+    'SEPTORIA LEAF SPOT',
+    'SLS',
+    '• Also known as Septoria blight \n • Spreads rapidly and may quickly defoliate and weaken the plants \n • It can be controlled if detected early',
+    '1. LEAVES: \n  • Lower leaves are affected first, later it moves up the plant \n • The spots are smaller in size (1/16 to 18 inches) \n  • Diameter of spots is 1.6 to 3.2 mm in diameter \n  • Usually occurs in older leaves \n • May also appear on blossoms, calyxes, and stems \n  2. APPEARANCE: \n  Leaves appear yellow, then brown and eventually fall off \n  3. FRUIT: \n • The virus rarely affects fruits',
+    'MEANS OF INFECTION: \n • Lives on fallen tomato plant debris \n • Spreads by both wind and water \n • Damp conditions are favourable for virus to develop \n • Can travel long distances',
+    '',
+    '1. NON-CHEMICAL CONTROL: \n  • Remove plants with symptoms promptly and destroy infected areas \n  • Use crop rotation \n • Use disease free seeds \n  • Overhead watering should be avoided \n • Enough room should be provided for air circulation \n • Best to plant tomatoes in different location each other \n 2. CHEMICAL CONTROL: \n  • The plant can be protected by spraying copper oxychloride \n  • Use fungicidal sprays \n  • The infected leaves will not be protected by fungicides, but they will protect new leaves from becoming infected \n • For effective results the chemicals must be applied at 7-10 day intervals \n • Most commonly used chemicals: \n  o Maneb \n  o Chlorothalonil',
+    ''
+);
