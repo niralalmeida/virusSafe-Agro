87c7ed304dcc45e40a7cdebfe94ec1c500c1d6bc
Haoyu Yang <hyan0050@student.monash.edu>
2020-09-21 22:12:39 +0800

    Add Insights fragment

--- VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/InsightsFragment.java
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/fragments/InsightsFragment.java
@@ -19,24 +19,25 @@ import com.example.virussafeagro.uitilities.MyAnimationBox;
 
 import java.util.Objects;
 
-public class ControlStrategiesFragment extends Fragment {
+public class InsightsFragment extends Fragment {
     private View view;
 
     private RelativeLayout allViewsRelativeLayout;
-    private RelativeLayout chemicalRelativeLayout;
-    private RelativeLayout nonChemicalRelativeLayout;
+    private RelativeLayout newsRelativeLayout;
+    private RelativeLayout tweetsRelativeLayout;
+    private RelativeLayout videosRelativeLayout;
 
-    public ControlStrategiesFragment() {
+    public InsightsFragment() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the View for this fragment
-        this.view = inflater.inflate(R.layout.fragment_control_strategies, container, false);
+        this.view = inflater.inflate(R.layout.fragment_insights, container, false);
 
         // set title
-        Objects.requireNonNull(Objects.requireNonNull((AppCompatActivity) getActivity()).getSupportActionBar()).setTitle("Control Strategies");
+        Objects.requireNonNull(Objects.requireNonNull((AppCompatActivity) getActivity()).getSupportActionBar()).setTitle("Insights");
 
         // show back button
         MainActivity.showTopActionBar((MainActivity)requireActivity());
@@ -59,9 +60,10 @@ public class ControlStrategiesFragment extends Fragment {
     }
 
     private void initializeViews() {
-        this.allViewsRelativeLayout = view.findViewById(R.id.rl_all_views_control);
-        this.chemicalRelativeLayout = view.findViewById(R.id.rl_chemical_control);
-        this.nonChemicalRelativeLayout = view.findViewById(R.id.rl_non_chemical_control);
+        this.allViewsRelativeLayout = view.findViewById(R.id.rl_all_views_insights);
+        this.newsRelativeLayout = view.findViewById(R.id.rl_news_insights);
+        this.tweetsRelativeLayout = view.findViewById(R.id.rl_tweets_insights);
+        this.videosRelativeLayout = view.findViewById(R.id.rl_videos_insights);
     }
 
     // show All Views
@@ -70,23 +72,32 @@ public class ControlStrategiesFragment extends Fragment {
     }
 
     private void allTilesOnClickListener() {
-        // chemical
-        this.setChemicalTileOnClickListener();
-        // non-chemical
-        this.setNonChemicalTileOnClickListener();
+        // news
+        this.setNewsTileOnClickListener();
+        // tweets
+        this.setTweetsTileOnClickListener();
+        // videos
+        this.setVideosTileOnClickListener();
     }
 
-    // chemical
-    private void setChemicalTileOnClickListener() {
-        this.chemicalRelativeLayout.setOnClickListener(llView -> {
+    // news
+    private void setNewsTileOnClickListener() {
+        this.newsRelativeLayout.setOnClickListener(llView -> {
             FragmentOperator.replaceFragment(requireActivity(), new NewsFragment(), AppResources.FRAGMENT_TAG_NEWS_LIST);
         });
     }
 
-    // non-chemical
-    private void setNonChemicalTileOnClickListener() {
-        this.nonChemicalRelativeLayout.setOnClickListener(llView -> {
-            FragmentOperator.replaceFragment(requireActivity(), new NewsFragment(), AppResources.FRAGMENT_TAG_NEWS_LIST);
+    // tweets
+    private void setTweetsTileOnClickListener() {
+        this.newsRelativeLayout.setOnClickListener(llView -> {
+//            FragmentOperator.replaceFragment(requireActivity(), new NewsFragment(), AppResources.FRAGMENT_TAG_NEWS_LIST);
+        });
+    }
+
+    // videos
+    private void setVideosTileOnClickListener() {
+        this.newsRelativeLayout.setOnClickListener(llView -> {
+//            FragmentOperator.replaceFragment(requireActivity(), new NewsFragment(), AppResources.FRAGMENT_TAG_NEWS_LIST);
         });
     }
 }
