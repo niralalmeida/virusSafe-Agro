a93b4bf3650e2924351936afb6268f47f75c603e
Haoyu Yang <hyan0050@student.monash.edu>
2020-10-12 16:11:56 +0800

    Add transition animation for the 2 buttons in quiz activity

--- VirusSafeAgro/app/src/main/java/com/example/virussafeagro/QuizActivity.java
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.virussafeagro;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.core.app.ActivityOptionsCompat;
 import androidx.core.view.ViewCompat;
 
@@ -11,15 +12,22 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.virussafeagro.models.VirusModel;
 import com.example.virussafeagro.uitilities.AppResources;
+import com.example.virussafeagro.uitilities.MyAnimationBox;
 
 public class QuizActivity extends AppCompatActivity {
     private int currentVirusModelId;
+
+    // views
+    private MotionLayout containerMotionLayout;
     private ImageView virusImageView;
+    private Button beginnerButton;
+    private Button intermediateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +46,10 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void initializeViews() {
-        virusImageView = findViewById(R.id.img_pic_virus_quiz);
+        this.containerMotionLayout = findViewById(R.id.ml_container_quiz_activity);
+        this.virusImageView = findViewById(R.id.img_pic_virus_quiz_activity);
+        this.beginnerButton = findViewById(R.id.btn_beginner_quiz_activity);
+        this.intermediateButton = findViewById(R.id.btn_intermediate_quiz_activity);
     }
 
     private void showActivityViews() {
@@ -46,6 +57,30 @@ public class QuizActivity extends AppCompatActivity {
         int virusPictureDrawableId = AppResources.getVirusPictureDrawableId(currentVirusModelId);
         Bitmap virusPictureBitmap = BitmapFactory.decodeResource(getResources(), virusPictureDrawableId);
         this.virusImageView.setImageBitmap(virusPictureBitmap);
+    }
+
+    public void beginnerOnClick(View v){
+//        MyAnimationBox.runFadeOutAnimation(intermediateButton, 200);
+
+        if (!beginnerButton.getText().toString().equals("Start Quiz")) {
+            intermediateButton.setVisibility(View.GONE);
+            containerMotionLayout.clearAnimation();
+            containerMotionLayout.setTransition(R.id.start_beginner, R.id.end_beginner);
+            containerMotionLayout.transitionToEnd();
+            beginnerButton.setText("Start Quiz");
+        }
+    }
+
+    public void intermediateOnClick(View v){
+//        MyAnimationBox.runFadeOutAnimation(beginnerButton, 200);
+
+        if (!intermediateButton.getText().toString().equals("Start Quiz")) {
+            beginnerButton.setVisibility(View.GONE);
+            containerMotionLayout.clearAnimation();
+            containerMotionLayout.setTransition(R.id.start_intermediate, R.id.end_intermediate);
+            containerMotionLayout.transitionToEnd();
+            intermediateButton.setText("Start Quiz");
+        }
     }
 
     public void close(View v) {
