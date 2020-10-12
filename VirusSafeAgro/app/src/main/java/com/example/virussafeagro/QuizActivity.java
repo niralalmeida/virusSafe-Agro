71d0140ce777c85edcf8c3a35f5c0a2d51335047
Haoyu Yang <hyan0050@student.monash.edu>
2020-10-12 18:35:31 +0800

    Change the other views' color in quiz activity

--- VirusSafeAgro/app/src/main/java/com/example/virussafeagro/QuizActivity.java
+++ VirusSafeAgro/app/src/main/java/com/example/virussafeagro/QuizActivity.java
@@ -4,16 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.virussafeagro.models.VirusModel;
@@ -25,9 +33,16 @@ public class QuizActivity extends AppCompatActivity {
 
     // views
     private MotionLayout containerMotionLayout;
+    private ImageButton closeImageButton;
+    private TextView quizTitleTextView;
+    private TextView virusFullNameTextView;
     private ImageView virusImageView;
     private Button beginnerButton;
     private Button intermediateButton;
+
+    // button names
+    private final String BUTTON_NAME_BEGINNER = "beginner";
+    private final String BUTTON_NAME_INTERMEDIATE = "intermediate";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +62,9 @@ public class QuizActivity extends AppCompatActivity {
 
     private void initializeViews() {
         this.containerMotionLayout = findViewById(R.id.ml_container_quiz_activity);
+        this.closeImageButton = findViewById(R.id.imgbtn_close_quiz_activity);
+        this.quizTitleTextView = findViewById(R.id.tv_title_quiz_activity);
+        this.virusFullNameTextView = findViewById(R.id.tv_virus_full_name_quiz_quiz_activity);
         this.virusImageView = findViewById(R.id.img_pic_virus_quiz_activity);
         this.beginnerButton = findViewById(R.id.btn_beginner_quiz_activity);
         this.intermediateButton = findViewById(R.id.btn_intermediate_quiz_activity);
@@ -60,27 +78,49 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void beginnerOnClick(View v){
-//        MyAnimationBox.runFadeOutAnimation(intermediateButton, 200);
-
         if (!beginnerButton.getText().toString().equals("Start Quiz")) {
-            intermediateButton.setVisibility(View.GONE);
+            // move beginner Button + hide intermediate Button
             containerMotionLayout.clearAnimation();
             containerMotionLayout.setTransition(R.id.start_beginner, R.id.end_beginner);
             containerMotionLayout.transitionToEnd();
             beginnerButton.setText("Start Quiz");
+            // change other views' style
+            changeOtherViewsStyle(BUTTON_NAME_BEGINNER);
         }
     }
 
     public void intermediateOnClick(View v){
-//        MyAnimationBox.runFadeOutAnimation(beginnerButton, 200);
-
         if (!intermediateButton.getText().toString().equals("Start Quiz")) {
-            beginnerButton.setVisibility(View.GONE);
+            // move intermediate Button + hide beginner Button
             containerMotionLayout.clearAnimation();
             containerMotionLayout.setTransition(R.id.start_intermediate, R.id.end_intermediate);
             containerMotionLayout.transitionToEnd();
             intermediateButton.setText("Start Quiz");
+            // change other views' style
+            changeOtherViewsStyle(BUTTON_NAME_INTERMEDIATE);
         }
+    }
+
+    private void changeOtherViewsStyle(String buttonName) {
+        // change the background
+        ColorDrawable[] colorDrawablesForBG = {
+                new ColorDrawable(getResources().getColor(R.color.colorPrimaryDarkBG)),
+                new ColorDrawable(getResources().getColor(R.color.colorWhite))};
+        TransitionDrawable transitionDrawable = new TransitionDrawable(colorDrawablesForBG);
+        containerMotionLayout.setBackground(transitionDrawable);
+        transitionDrawable.startTransition(300);
+        // change the quiz title color
+        if(buttonName.equals(BUTTON_NAME_BEGINNER)) {
+            quizTitleTextView.setTextColor(getResources().getColor(R.color.btn_beginner_bg));
+        } else if(buttonName.equals(BUTTON_NAME_INTERMEDIATE)){
+            quizTitleTextView.setTextColor(getResources().getColor(R.color.btn_intermediate_bg));
+        }
+        // change the virus full name color
+        virusFullNameTextView.setTextColor(getResources().getColor(R.color.colorPrimaryDarkBG));
+        // change the close button color
+        ColorStateList colorStateList = ContextCompat.getColorStateList(getApplicationContext(), R.color.colorPrimaryDarkBG);
+        closeImageButton.setImageTintMode(PorterDuff.Mode.SRC_ATOP);
+        closeImageButton.setImageTintList(colorStateList);
     }
 
     public void close(View v) {
